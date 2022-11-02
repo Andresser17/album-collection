a840f7008147ef11e61f0a20579f611ebf2f4403
Andresser17 <andresserserrano2020@gmail.com>
2022-11-02 12:08:56 -0400

    style: fix css errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 // Sections
 import Header from "sections/Header";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Header />
+        <Outlet />
+      </div>
+    </div>
   );
 }
 
